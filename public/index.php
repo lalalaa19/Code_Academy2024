58d428aed14c6e5dda9fcb65bab92a9b10b86305
lalalaa19 <violaren19@gmail.com>
2024-12-30 23:16:53 +0700

    Semoga berhasil

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Contracts\Http\Kernel;
 
 define('LARAVEL_START', microtime(true));
 
@@ -17,7 +18,7 @@ require __DIR__.'/../vendor/autoload.php';
 //     ->handleRequest(Request::capture());
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+$kernel = $app->make(Kernel::class);
 $response = $kernel->handle(
     $request = Request::capture()
 )->send();
