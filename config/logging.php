7a4f304db47a4ef4d434c87c69e6c3df41370eb2
lalalaa19 <violaren19@gmail.com>
2024-12-30 23:33:49 +0700

    Semoga berhasil

--- config/logging.php
+++ config/logging.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'errorlog'),
+    'default' => env('LOG_CHANNEL', 'stack'),
 
     /*
     |--------------------------------------------------------------------------
